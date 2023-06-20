96f675b99cec2ec48be76d3b85e73cc185af356c
nandaadyatma <putunanda928@gmail.com>
2023-06-21 01:40:00 +0800

    menambahkan layout login, regis, dan lengkapi profil

---
+++ app/src/main/java/com/example/tugasreal/Registrasi.kt
@@ -0,0 +1,11 @@
+package com.example.tugasreal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registrasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrasi)
+    }
+}
