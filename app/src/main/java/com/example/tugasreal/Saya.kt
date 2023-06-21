a6e648027474f4669ec92b170a634dd0b40a1094
dewisnu <wisnuari4658@gmail.com>
2023-06-21 20:00:16 +0800

    fix:few error

--- app/src/main/java/com/example/tugasreal/Saya.kt
+++ app/src/main/java/com/example/tugasreal/Saya.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +38,7 @@ class Saya : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_saya, container, false)
+
     }
 
     companion object {
