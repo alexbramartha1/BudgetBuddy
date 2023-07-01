7066fba1ef3d683c0008d13c4c46f0c1418064da
wiratana <andikawiratana@gmail.com>
2023-07-01 18:21:08 +0700

    basic store exchange

---
+++ app/src/main/java/com/example/tugasreal/Exchange.kt
@@ -0,0 +1,12 @@
+package com.example.tugasreal
+
+data class Exchange(
+    val userID: String? = null,
+    val amount: Int? = null,
+    val type: String? = null,
+    val category: String? = null,
+    val date: String? = null,
+    val desc: String? = null
+){
+
+}
--- app/src/main/java/com/example/tugasreal/Transaksi.kt
+++ app/src/main/java/com/example/tugasreal/Transaksi.kt
@@ -5,6 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.tugasreal.databinding.ActivityMainBinding
+import com.example.tugasreal.databinding.FragmentTransaksiBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +29,10 @@ class Transaksi : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var database:DatabaseReference
+    private var _binding: FragmentTransaksiBinding? = null
+    private val binding get() = _binding!!
+    private var type:String = "income"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +40,63 @@ class Transaksi : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        database = Firebase.database.reference
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaksi, container, false)
+        _binding = FragmentTransaksiBinding.inflate(inflater, container, false)
+        var dummyEmail = "andika wiratana"
+        binding.saveExchange.setOnClickListener {
+            var amount      = Integer.parseInt(binding.amount.text.toString())
+            var category    = binding.category.text.toString()
+            var date        = binding.date.text.toString()
+            var description = binding.description.text.toString()
+            this.insert(dummyEmail, amount, this.type, category, date, description)
+        }
+
+        binding.incomeButton.setOnClickListener {
+            this.type = "income"
+            println(this.type)
+            this.setFeatureButton()
+        }
+
+        binding.expanseButton.setOnClickListener {
+            this.type = "expanse"
+            println(this.type)
+            this.setFeatureButton()
+        }
+
+        val view = binding.root
+        return view
+    }
+
+    fun setFeatureButton(){
+        val buttons = arrayListOf(binding.incomeButton, binding.expanseButton)
+        //Reset buttons theme
+        for(button in buttons){
+            button.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+            button.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+        }
+        when(this.type){
+            "income" -> {
+                binding.incomeButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.blue_light))
+                binding.incomeButton.setTextColor(ContextCompat.getColor(requireContext(), R.color.blue_dark))
+            }
+            "expanse" -> {
+                binding.expanseButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.blue_light))
+                binding.expanseButton.setTextColor(ContextCompat.getColor(requireContext(), R.color.blue_dark))
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
@@ -55,5 +117,18 @@ class Transaksi : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    fun insert(userID: String, amount: Int, type: String, category: String, date: String, desc: String){
+        var exchange = Exchange(userID, amount, type, category, date, desc)
+        var key      = this.database.child("exchange").push().getKey()
+        key?.let {
+            this.database.child("exchange").child(it).setValue(exchange).addOnCompleteListener{ task ->
+                if (task.isSuccessful){
+                    Toast.makeText(context, "Succcessfull saving exchange!", Toast.LENGTH_LONG)
+                    println("success")
+                }
+            }
+        }
     }
 }
