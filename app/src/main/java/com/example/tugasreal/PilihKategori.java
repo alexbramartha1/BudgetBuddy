a1cd0116b256e869379d1812d7c8570e57075ca8
Putu Nanda Arya Adyatma <putunanda928@gmail.com>
2023-07-01 19:12:15 +0800

    Add pilihKategori Layout

---
+++ app/src/main/java/com/example/tugasreal/PilihKategori.java
@@ -0,0 +1,14 @@
+package com.example.tugasreal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PilihKategori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_kategori);
+    }
+}
