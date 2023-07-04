2662656fdd3a493d7ea7ce1e443de895f5eb310d
dewisnu <wisnuari4658@gmail.com>
2023-07-05 03:17:15 +0800

    feat: add username in berada

--- app/src/main/java/com/example/tugasreal/Beranda.kt
+++ app/src/main/java/com/example/tugasreal/Beranda.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tugasreal.databinding.FragmentBerandaBinding
+import com.example.tugasreal.databinding.FragmentSayaBinding
+import com.google.firebase.auth.FirebaseAuth
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -27,8 +30,10 @@ class Beranda : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda, container, false)
+        val binding = FragmentBerandaBinding.inflate(inflater,container,false)
+        binding.header.text = "Halo ${FirebaseAuth.getInstance().currentUser?.displayName}"
+
+        return binding.root
     }
 
 
